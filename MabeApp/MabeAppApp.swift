083e723b6585a32cf45cc2626a8277c73a709124
Rafa <rafaelmejia2005@gmail.com>
2026-05-05 01:09:45 -0600

    Initial Commit

---
+++ MabeApp/MabeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MabeAppApp.swift
+//  MabeApp
+//
+//  Created by Rafael on 05/05/26.
+//
+
+import SwiftUI
+
+@main
+struct MabeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
